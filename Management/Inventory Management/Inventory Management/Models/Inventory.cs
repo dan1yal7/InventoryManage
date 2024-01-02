82fced4ce6446b20d291b5dd3eadab0210ff2552
dan1yal7 <daniiltemirkhanov@gmail.com>
2024-01-02 22:21:04 +0500

    InventoryLogic
    Задача - создать класс Inventory, который будет отслеживать товары на складе. Каждый товар имеет следующие характеристики: название, количество в наличии и цена.
    
    Создайте класс Item, представляющий товар с полями для названия, количества и цены.
    
    Создайте класс Inventory, который содержит список товаров. Реализуйте методы для:
    
    Добавления нового товара.
    Удаления товара по названию.
    Обновления количества товара.
    Расчета общей стоимости инвентаря.
    Вывода информации о всех товарах на складе.
    Реализуйте обработку исключений для следующих сценариев:
    
    Попытки добавить товар с отрицательным количеством или ценой.
    Удаления несуществующего товара.
    Обновления количества для несуществующего товара.

---
+++ Management/Inventory Management/Inventory Management/Models/Inventory.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Inventory_Management.Models
+{
+    public class Inventory
+    {
+        List<Item> items = new List<Item>();
+
+
+
+        /// <summary>
+        /// Добавление товара
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="quantity"></param>
+        /// <param name="price"></param>
+        /// <exception cref="ArgumentException"></exception>
+
+        public void AddItem(string name, int quantity, decimal price)
+        {
+            if (quantity < 0 || price < 0)
+
+                throw new ArgumentException("Quantity and price must be non-negative.");
+
+
+            Item newItem = new Item
+            {
+                Name = name,
+                Quantity = quantity,
+                Price = price,
+            };
+
+            items.Add(newItem);
+        }
+
+
+
+        /// <summary>
+        /// Удаление товара по названию
+        /// </summary>
+        /// <param name="name"></param>
+        /// <exception cref="ArgumentException"></exception>
+
+        public void RemoveItem(string name)
+        {
+            Item ItemReomove = items.Find(item => item.Name.Equals( name, StringComparison.OrdinalIgnoreCase));
+
+            if (ItemReomove != null)
+            {
+                items.Remove(ItemReomove);
+            }
+            else
+            {
+                throw new ArgumentException("Items not found");
+            }
+        }
+
+
+
+        /// <summary>
+        /// Обновление кол-во товара
+        /// </summary>
+        /// <param name="quantity"></param>
+        public void UpgradeItemQuantity(string name, int quantity)
+        {
+            Item ItemUpdate = items.Find(item => item.Name.Equals( name,StringComparison.OrdinalIgnoreCase));
+
+            if (ItemUpdate != null)
+            {
+                ItemUpdate.Quantity = quantity;
+            }
+
+            else
+            {
+                throw new ArgumentException("Item not found.");
+            }
+        }
+
+
+        /// <summary>
+        ///  Расчет общей стоимости инвентаря 
+        /// </summary>
+        /// <returns></returns>
+
+        public decimal CalcuatingValue()
+        {
+            decimal totalValue = 0;
+            foreach (Item item in items)
+            {
+                totalValue += item.Quantity * item.Price;
+
+            }
+            return totalValue;
+        }
+         
+
+        /// <summary>
+        ///  Вывод в консоль 
+        /// </summary>
+
+        public void Display()
+        {
+            Console.WriteLine("Inventory:");
+
+            foreach (Item item in items)
+            {
+                Console.WriteLine($"{item.Name} - Quantity: {item.Quantity} - Price:{item.Price:C}");
+            }
+
+            Console.WriteLine($"Total Value of Inventory: {CalcuatingValue():C}");
+        }
+    }
+}
---
+++ Management/Inventory Management/Inventory Management/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using Inventory_Management.Models;
+Inventory inventory = new Inventory();
+
+inventory.AddItem("item1", 10, 5.99m);
+inventory.AddItem("Item2", 5, 8.49m);
+
+// Вывод информации о товарах на складе
+inventory.Display();
+
+
+inventory.UpgradeItemQuantity("Item1", 15);
+
+// Вывод обновленной информации о товарах на складе
+inventory.Display();
+
+// Расчет общей стоимости инвентаря
+decimal totalValue = inventory.CalcuatingValue();
+Console.WriteLine($"Total Value of Inventory: {totalValue:C}");
+
+// Удаление товара
+inventory.RemoveItem("Item2");
+
+// Вывод информации после удаления товара
+inventory.Display();
